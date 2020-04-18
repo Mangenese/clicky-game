09eaab4a90cbbe7904ea9c94333f74edc4dbb0cd
Mangenese <blakemangen20@gmail.com>
2020-04-17 20:14:16 -0700

    Updates

---
+++ precache-manifest.a6935416faf122447784882de7bbb8cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75fc90def88b84d81ee1f27a623edfb9",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "539b3fa40891175ee8ac",
+    "url": "/clicky-game/static/css/main.18f456d8.chunk.css"
+  },
+  {
+    "revision": "85fb786ab42a7d15c636",
+    "url": "/clicky-game/static/js/2.e8fb3275.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.e8fb3275.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "539b3fa40891175ee8ac",
+    "url": "/clicky-game/static/js/main.f5cb1c92.chunk.js"
+  },
+  {
+    "revision": "ce865d6636edbcadb90f",
+    "url": "/clicky-game/static/js/runtime-main.fad383a3.js"
+  }
+]);
