04094c82c92097728d9dddc19cc0060527a1d679
Mangenese <blakemangen20@gmail.com>
2020-04-17 17:48:31 -0700

    changed file names for easy understanding and added the logic for the header

--- click/src/App.js
+++ click/src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import gods from "./gods.json";
+// import GodCard from "./components/GodCard"
+import Header from "./components/Header/Header";
+// import Footer from "./components/Footer";
+class App extends Component {
+  render() {
+    return (<div>
+      <Header score={this.state.score}
+      highscore={this.state.highScore}
+      />
+      {/* <GodCard /> */}
+      {/* <Footer /> */}
+    </div>)
+  }
 }
 
 export default App;
--- click/src/components/Header/Header.js
+++ click/src/components/Header/Header.js
@@ -1,2 +1,15 @@
 import React from "react";
 import "./Header.css"
+
+function Header(props) {
+    return <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+            <h1 className="display-4">Smite Memory Click Game!</h1>
+            <p className="lead">Click on the image to earn points, but dont click on a god mulitple times!</p>
+            <div>Score: {props.score}</div>
+            <div>High Score: {props.highScore}</div>
+        </div>
+    </div>
+}
+
+export default Header;
